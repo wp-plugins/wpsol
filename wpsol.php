11e02eff5fa7dbfd8e9c8a4c5b3537bd3ba208cb
koter84 <koter84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-11 08:41:58 +0000

    tagging version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/wpsol/tags/0.1@818741 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ wpsol.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: wpSOL
+Plugin URI: http://bitbucket.org/gerritjanf/wpsol
+Description: Connect WordPress to the Scouting Nederland OpenID Server
+Author: Gerrit Jan
+Author URI: http://gerritjanfaber.nl
+Version: 0.2
+License: 
+Text Domain: wpSOL
+*/
+
+require 'openid.php';
+require 'common.php';
+
+// Init wpsol-plugin
+function wpsol_init()
+{
+	// Gebruikersnaam veld toevoegen aan de login pagina
+	add_action( 'login_form', 'wpsol_wp_login_form' ); 
+	add_filter( 'login_form_middle', 'wpsol_wp_login_form_middle' ); 
+
+	// Inhaken op het authenticatie process
+	add_filter('authenticate',  'wpsol_authenticate_username_password', 9);
+
+	// Registreer sidebar widget
+	register_sidebar_widget('SOL Sidebar Login', 'wpsol_sidebar_login');
+}
+add_action("plugins_loaded", "wpsol_init");
+
+// Init wpsol-admin
+function wpsol_admin_menu() {
+	add_options_page( 'wpSOL', 'wpSOL', 'manage_options', 'wpsol_settings', 'wpsol_admin_options' );
+}
+add_action( 'admin_menu', 'wpsol_admin_menu' );
